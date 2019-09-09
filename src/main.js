94bd00d3e9c424f06eb7c20c36ce533ca3555c4d
HackXIt <hackxit@gmail.com>
2019-09-09 21:39:09 +0200

    Removed vue-polling from branch

--- src/main.js
+++ src/main.js
@@ -4,11 +4,9 @@ import router from "./router.js";
 import VueAwesomeCountdown from "vue-awesome-countdown";
 import VueStatusIndicator from "vue-status-indicator";
 import VueDraggableResizable from "vue-draggable-resizable";
-import VuePolling from "vue-polling";
 
 Vue.use(VueAwesomeCountdown);
 Vue.use(VueStatusIndicator);
-Vue.use(VuePolling);
 Vue.component("vue-draggable-resizable", VueDraggableResizable);
 Vue.config.productionTip = false;
 
